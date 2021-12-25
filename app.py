1a1b567b5a10d8e49592f733c204b725e7b479d1
yoririn <87194790+yoririn@users.noreply.github.com>
2021-12-25 23:46:39 +0900

    editing 'up_item'

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import StringVar, ttk
+from tkinter import font
 from tkinter import filedialog
 from tkinter.messagebox import showerror, showwarning
-
+from PyPDF2 import PdfFileReader 
+from pdf2image import convert_from_path
 # import pandas as pd
 # import numpy as np
 import sys
@@ -32,36 +34,72 @@ class InputFrame(ttk.Frame):
             weight=1,
         )
 
-        # filepath entry
         self.filepath = tk.StringVar()
-        self.filepath_entry = ttk.Entry(self.browse_frame, textvariable=self.filepath)
-        self.filepath_entry.grid(column=0, row=0, sticky=tk.EW)
-        self.filepath_entry.focus()
+        filepath_entry = ttk.Entry(self.browse_frame, textvariable=self.filepath)
+        filepath_entry.grid(column=0, row=0, sticky=tk.EW)
+        filepath_entry.focus()
 
-        # browse button
         self.img_mglass = tk.PhotoImage(file=resource_path("mglass.png"))
-        self.load_button = ttk.Button(
+        load_button = ttk.Button(
             self.browse_frame, image=self.img_mglass, command=self.browse_file
         )
-        self.load_button.grid(column=1, row=0)
+        load_button.grid(column=1, row=0)
+
+        # pdf view frame
+        self.pdf_frame=ttk.Frame(self)
+        self.pdf_frame.grid(column=0, row=1, sticky=tk.EW, ipadx=5, ipady=3)
+        self.pdf_frame.columnconfigure(
+            [0],
+            weight=1,
+        )
+
+        self.pagenum_list=tk.StringVar(value=[])
+        self.pages=tk.Listbox(self.pdf_frame,listvariable=self.pagenum_list)
+        self.pages.grid(column=0,row=0,sticky=tk.EW)
+
+        button_frame=ttk.Frame(self.pdf_frame)
+        button_frame.grid(column=1,row=0)
+
+        up_button = ttk.Button(button_frame, text="⬆", command=self.up_item)
+        up_button.grid(row=0, column=0)
+
+        down_button = ttk.Button(button_frame, text="⬇", command=self.down_item)
+        down_button.grid(row=1, column=0)
 
     def browse_file(self):
         """
         Handle button click event
         """
-        self.filepath = filedialog.askopenfilename(
+        self.filepath.set(filedialog.askopenfilename(
             filetypes=[("pdf file", "*.pdf"), ("all", "*")],
             title="load",
-        )
+        ))
         # self.df=pd.read_csv(f'{self.filepath}',encoding='shift_jis',header=None)
-        self.filepath_entry.delete(0, tk.END)
-        self.filepath_entry.insert(tk.END, self.filepath)
+        # self.filepath_entry.delete(0, tk.END)
+        # self.filepath_entry.insert(tk.END, self.filepath)
+        self.pdf=pdf=PdfFileReader(self.filepath.get())
+        num_pages=pdf.getNumPages()
+        self.pagenum_list.set([i for i in range(num_pages)])
+    
+    def up_item(self):
+        selected=int(self.pages.curselection()[0])
+        if selected!=0:
+            _list=self.pagenum_list.get()
+            print(_list)
+            _list[selected-1],_list[selected]=_list[selected],_list[selected-1]
+            self.pagenum_list.set(_list)
+
+    def down_item(self):
+        pass
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("")
+        default_font = font.nametofont("TkDefaultFont")
+        default_font.configure(family="Arial",weight="bold")
+        
         self.attributes("-alpha", 0.0)
         self.resizable(width=True, height=False)
         self.minsize(width=400, height=20)
